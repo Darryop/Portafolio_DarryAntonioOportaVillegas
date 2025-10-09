cdc100310dbb20807739ab35ee8079938367c904
Darryop <darryale6@gmail.com>
2025-10-08 18:17:20 -0600

    arreglo de semana 3
    semana 3 ya funcional

---
+++ Tienda_Darry/src/main/java/Tienda/demo/repository/CategoriaRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tienda.demo.repository;
+import Tienda.demo.domain.Categoria;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author bryan
+ */
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer>{
+    public List<Categoria> findByActivoTrue();
+}
